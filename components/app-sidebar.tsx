44bc7840ba01b103729c1557bd3c25ceca274acf
Drew <tullos.andrew@gmail.com>
2026-08-23 13:05:36 -0500

    Sidebar

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -1,20 +1,64 @@
 import {
-   Sidebar,
-   SidebarContent,
-   SidebarFooter,
-   SidebarGroup,
-   SidebarHeader,
+  Sidebar,
+  SidebarHeader,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
 } from "@/components/ui/sidebar"
 
+const items = [
+  {
+    title: "Dashboard",
+    url: "#",
+  },
+  {
+    title: "Water Chemistry",
+    url: "#",
+  },
+  {
+    title: "Maintenance",
+    url: "#",
+  },
+    {
+    title: "Trends",
+    url: "#",
+  },
+  {
+    title: "Live Stock",
+    url: "#",
+  },
+    {
+    title: "Settings",
+    url: "#",
+  },
+]
+
 export function AppSidebar() {
-   return (
-      <Sidebar>
-      <SidebarHeader />
+  return (
+    <Sidebar>
+         <SidebarHeader>
+          <SidebarGroupLabel>Reef Dashboard</SidebarGroupLabel>
+         </SidebarHeader>
       <SidebarContent>
-         <SidebarGroup />
-         <SidebarGroup />
+        <SidebarGroup>
+
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton render={<a href={item.url} />}>
+                    <span>{item.title}</span>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
       </SidebarContent>
-      <SidebarFooter />
-      </Sidebar>
-   )
+    </Sidebar>
+  )
 }
